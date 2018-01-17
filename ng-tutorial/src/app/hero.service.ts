635d611cd474698a9a4b0f331e13b91abaf4de68
Javier Abadía <javi.abadia@gmail.com>
2018-01-17 10:51:53 +0100

    tour of heroes Angular tutorial

---
+++ ng-tutorial/src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {of} from "rxjs/observable/of";
+
+import {Hero} from "./hero";
+import {HEROES} from "./mock-heroes";
+import {MessageService} from "./message.service";
+
+@Injectable()
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes() : Observable<Hero[]> {
+    this.messageService.add('Hero Service: fetched heroes');
+    return of(HEROES);
+  }
+
+}
---
+++ ng-tutorial/src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService} from "../hero.service";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes : Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+}
